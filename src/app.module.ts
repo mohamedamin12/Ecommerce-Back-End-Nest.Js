7f2ae508edabaec472215d7249ab693758a31b43
mohamedamin12 <aboamin909080@gmail.com>
2025-03-21 14:48:19 +0200

    connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/E-commerce'), UserModule],
+
 })
 export class AppModule {}
